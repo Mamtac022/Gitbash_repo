7ebc265cc8abbb60fec3f84c47eacc1e8d1bc544
Mamtac022 <mamtac022@gmail.com>
2024-08-16 15:37:03 +0530

    First Push

---
+++ src/test/java/pac2/Org.java
@@ -0,0 +1,18 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class Org {
+	@Test
+	public void createOrg() {	
+	System.out.println("execute create organization test");
+	}
+	
+		@Test
+		public void modifyOrgTest() {
+			System.out.println("execute modify  organization test");
+			
+		}
+	}
+
+
